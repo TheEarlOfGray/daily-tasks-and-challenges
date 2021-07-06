9e0935168ce2790bba688d3ffa198b6e63c26210
Earl <theearlofgray@protonmail.com>
2021-07-06 12:38:16 +0000

    EG: updated times tables with function

--- easy/times_table.py
+++ easy/times_table.py
@@ -1,7 +1,12 @@
 print("Welcome to the times table calculator")
 req = int(input("Please provide a number: "))
 
-for i in range(1, req + 1):
-    for j in range(1, 11):
-        print(j * i, end=" ")
-    print("\n")
+
+def print_times_tables(req):
+    for i in range(1, req + 1):
+        for j in range(1, 11):
+            print(j * i, end=" ")
+        print("\n")
+
+
+print_times_tables(req)
